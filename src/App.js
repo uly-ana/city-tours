4adfc5e57b0e31653bed32453d2861de9c53f063
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-06 21:00:55 +0300

    changed css files to scss, replaced logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-npm
 
 function App() {
   return (
     <div className="City Tours">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>City Tours app</h1>
     </div>
   );
 }
