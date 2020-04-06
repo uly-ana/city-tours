8691fd648be51676cc9bc416367156d090226fb9
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-06 16:15:07 +0300

    added font awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+npm
 
 function App() {
   return (
-    <div className="App">
+    <div className="City Tours">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
