5145bfc5b1f61667649bd57e3729ea9fd5d0afc5
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-08 00:37:35 +0300

    added tour component, logic and styled it

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import './tour.scss'
+
+
 export default class Tour extends Component {
     render() {
+
+        const {id, city, img, info, name} = this.props.tour;
+        const {removeTour} = this.props;
+
         return (
-            <div>
-                Hello from tour
+          <article className="tour">
+            <div className="img-container">
+              <img src={img} alt="tour image" />
+              <span className="close-btn">
+                <i className="fas fa-window-close" />
+              </span>
             </div>
-        )
+            <div className="tour-info">
+              <h3>{city}</h3>
+              <h4>{name}</h4>
+              <h5>
+                More info{" "}
+                <span>
+                  <i class="fas fa-caret-square-down"></i>
+                </span>
+              </h5>
+              <p className="overflow-ellipsis">{info}</p>
+            </div>
+          </article>
+        );
     }
 }
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import Tour from '../Tour';
 import './tourlist.scss';
+import {tourData} from '../../tourData';
 
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData
+    };
+    removeTour = id => {
+    console.log(id);
+
+    }
+
+
     render() {
+        console.log(this.state.tours);
+        const {tours} = this.state;
         return (
-            <div>
-                Hello from tour list
-                <Tour />
-            </div>
+            <section className="tourlist">
+                {
+                    tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>))
+                }
+                
+            </section>
         );
     }
 }
