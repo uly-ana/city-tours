d935467cce271a3076cbad76cc6f1ef28d77a728
Ulyana Melnyk <ulianamelnyk@gmail.com>
2020-04-07 20:11:01 +0300

    added tours data

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,23 +5,38 @@ import logo from '../../logo.png'
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
-            <a href="#">
-                <img src={logo} alt="city tours logo" style={{ width: 60, height: 50 }}></img><text id="brand">C-Tours</text></a>
-            <ul className="nav-links">
-                <li><a href="/" className="nav-link">
-                    About Us
-                </a></li>
-                <li><a href="/" className="nav-link active ">
-                    Tours
-                </a></li>
-                <li><a href="/" className="nav-link">
-                    Blog
-                </a></li>
-                <li><a href="/" className="nav-link">
-                    Contact Us
-                </a></li>
-            </ul>
-        </nav>
-    )
+      <nav className="navbar">
+        <a href="#">
+          <img
+            src={logo}
+            alt="city tours logo"
+            style={{ width: 60, height: 60 }}
+          ></img>{" "}
+          <text id="brand">Tours</text>
+        </a>
+
+        <ul className="nav-links">
+          <li>
+            <a href="/" className="nav-link">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="/" className="nav-link active ">
+              Distanations
+            </a>
+          </li>
+          <li>
+            <a href="/" className="nav-link">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="/" className="nav-link">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
 }
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Tour from '../Tour';
+import './tourlist.scss';
+
 
 export default class TourList extends Component {
     render() {
